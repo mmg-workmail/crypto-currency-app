3cd52a9e78eb218811206b1f4dbfbd690a8255ec
mmg <mohamad@gmail.com>
2024-07-11 13:29:09 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const MyMaterialApp());
+}
+
+class MyMaterialApp extends StatefulWidget {
+  const MyMaterialApp({super.key});
+
+  @override
+  State<MyMaterialApp> createState() => _MyMaterialAppState();
+}
+
+class _MyMaterialAppState extends State<MyMaterialApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+          ),
+          body: Container(),
+        ),
+      ),
+    );
+  }
+}
